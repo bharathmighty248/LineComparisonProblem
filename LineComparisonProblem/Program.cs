a8ffc143a4fdfea50105a7b11f4c4f9a9ec2dad0
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-22 16:45:40 +0530

    [Bharath] Add: Comparison Check of Two Lines Implemented

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -7,13 +7,15 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Length line1 = new Length(3, 2, 7, 5);
-            Length line2 = new Length(3, 2, 7, 5);
+            Length line2 = new Length(3, 2, 8, 5);
             int line1Length = Convert.ToInt32(line1.CalculateLength());
             int line2Length = Convert.ToInt32(line2.CalculateLength());
-            if (line1Length.Equals(line2Length))
-                Console.WriteLine("Both Lines are Equal");
+            if (line1Length.CompareTo(line2Length) >0)
+                Console.WriteLine("Line 1 is Greaterthan Line 2");
+            else if (line1Length.CompareTo(line2Length) < 0)
+                Console.WriteLine("Line 1 is Lessthan Line 2");
             else
-                Console.WriteLine("Both Lines are Not Equal");
+                Console.WriteLine("Both Lines are Equal");
         }
     }
 }

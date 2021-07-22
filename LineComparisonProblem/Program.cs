b9de6950035e5d099ac0c4873f41319f9fb6ec19
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-22 16:36:12 +0530

    [Bharath] Add: Equality Check Logic Implemented

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -6,9 +6,14 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Length line = new Length(3, 2, 7, 5);
-            int lineLength = Convert.ToInt32(line.CalculateLength());
-            Console.WriteLine("Length of the line: " + lineLength);
+            Length line1 = new Length(3, 2, 7, 5);
+            Length line2 = new Length(3, 2, 7, 5);
+            int line1Length = Convert.ToInt32(line1.CalculateLength());
+            int line2Length = Convert.ToInt32(line2.CalculateLength());
+            if (line1Length.Equals(line2Length))
+                Console.WriteLine("Both Lines are Equal");
+            else
+                Console.WriteLine("Both Lines are Not Equal");
         }
     }
 }

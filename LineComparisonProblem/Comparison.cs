afa5186406c0c60f5422e129d7e2fd1ef6b2f9fa
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-26 15:13:24 +0530

    [Bharath] Refactor: Using Class Method CompareTo Implemented

---
+++ LineComparisonProblem/Comparison.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonProblem
+{
+    class Comparison
+    {
+        public void CompareTo()
+        {
+            Length line1 = new Length(3, 2, 7, 5);
+            Length line2 = new Length(3, 2, 8, 5);
+            int line1Length = Convert.ToInt32(line1.CalculateLength());
+            int line2Length = Convert.ToInt32(line2.CalculateLength());
+            if (line1Length.CompareTo(line2Length) > 0)
+                Console.WriteLine("Line 1 is Greaterthan Line 2");
+            else if (line1Length.CompareTo(line2Length) < 0)
+                Console.WriteLine("Line 1 is Lessthan Line 2");
+            else if (line1Length.CompareTo(line2Length) == 0)
+                Console.WriteLine("Both Lines are Equal");
+        }
+    }
+}
